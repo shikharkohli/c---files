33577a0d753ef7334c05636ac1a879d23a4aec6a
Shikhar Kohli <shikharkohli@gmail.com>
2011-07-21 22:34:12 +0530

    spoj

---
+++ spoj/rootciph.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int t;
+    long long int a,b,c;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%lld %lld %lld",&a,&b,&c);
+        printf("%lld\n",a*a-2*b);
+    }
+    return 0;
+}
